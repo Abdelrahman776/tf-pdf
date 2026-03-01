1416e5c3af97f0eaa3b46ee98677dd6b4eeb4b35
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2026-03-01 18:05:36 +0100

    Refactor Navbar component: improve layout, update logo size, and enhance button styles

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { CreditCardIcon, MenuIcon, XIcon, LogIn } from "lucide-react";
-import ThemeToggle from "./ThemeToggle";
+import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle.tsx";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -24,30 +25,30 @@ export default function Navbar() {
   }, [theme]);
 
   return (
-    <nav className="sticky top-0 z-50 bg-tfwhite dark:bg-tfblack border-b border-gray-200/80 dark:border-gray-900">
+    <nav className="sticky top-0 z-50 bg-tfwhite dark:bg-tfblack  dark:border-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          <div className="flex items-center gap-1.5">
+        <div className="flex justify-between items-center h-16">
+          <Link to="/" className="flex items-center gap-1.5" aria-label="Go to home">
             <img
               src="/tf-pdf-logo.png"
               alt="tf pdf logo"
               draggable="false"
-              className="w-8 h-8 object-contain"
+              className="w-9 h-9 object-contain"
             />
             <div className="text-[1.65rem] leading-none font-bold tracking-tight">
               <span className="text-tfblue">TrueForm</span>
               <span className="text-tforange">PDF</span>
             </div>
-          </div>
+          </Link>
 
-          <div className="hidden sm:flex items-center space-x-3">
+          <div className="hidden sm:flex items-center space-x-3 h-full">
             <ThemeToggle theme={theme} setTheme={setTheme} />
-            <button className="flex items-center px-3 py-2 rounded-2xl text-[0.85rem] text-tfblack dark:text-tfwhite hover:bg-gray-300 dark:hover:bg-gray-700">
+            <button className="h-9 flex items-center px-3 rounded-2xl text-[0.85rem] text-tfblack dark:text-tfwhite hover:bg-gray-300 dark:hover:bg-gray-700">
               <CreditCardIcon className="w-5 h-5 mr-1" />
               <p>Credits: N/A</p>
             </button>
-            <button className="px-5 py-2 rounded-2xl text-tfwhite dark:text-tfwhite bg-tfblue hover:bg-blue-950 transition-colors flex items-center text-sm font-semibold">
-              <LogIn className="mr-2 h-4 w-4 mt-0.5" />
+            <button className="h-9 px-5 rounded-2xl text-tfwhite dark:text-tfwhite bg-tfblue hover:bg-blue-950 transition-colors flex items-center text-sm font-semibold">
+              <LogIn className="mr-2 h-4 w-4" />
               <span>Sign In</span>
             </button>
           </div>
